ace8665c23733a9c08e358339c1bd86e5ed94aad
Seton9 <pissergah3@gmail.com>
2018-12-10 09:34:27 -0800

    Added Weapon Classes
    Make array, character components, and touch on animations (aim pitch)

---
+++ Deathmatch/Intermediate/Build/Win64/UE4Editor/Inc/Deathmatch/Shotgun.generated.h
@@ -0,0 +1,88 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef DEATHMATCH_Shotgun_generated_h
+#error "Shotgun.generated.h already included, missing '#pragma once' in Shotgun.h"
+#endif
+#define DEATHMATCH_Shotgun_generated_h
+
+#define Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_RPC_WRAPPERS
+#define Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAShotgun(); \
+	friend struct Z_Construct_UClass_AShotgun_Statics; \
+public: \
+	DECLARE_CLASS(AShotgun, AActor, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/Deathmatch"), NO_API) \
+	DECLARE_SERIALIZER(AShotgun)
+
+
+#define Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAShotgun(); \
+	friend struct Z_Construct_UClass_AShotgun_Statics; \
+public: \
+	DECLARE_CLASS(AShotgun, AActor, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/Deathmatch"), NO_API) \
+	DECLARE_SERIALIZER(AShotgun)
+
+
+#define Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AShotgun(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AShotgun) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AShotgun); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AShotgun); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AShotgun(AShotgun&&); \
+	NO_API AShotgun(const AShotgun&); \
+public:
+
+
+#define Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AShotgun(AShotgun&&); \
+	NO_API AShotgun(const AShotgun&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AShotgun); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AShotgun); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AShotgun)
+
+
+#define Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_PRIVATE_PROPERTY_OFFSET
+#define Deathmatch_Source_Deathmatch_Public_Shotgun_h_9_PROLOG
+#define Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_PRIVATE_PROPERTY_OFFSET \
+	Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_RPC_WRAPPERS \
+	Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_INCLASS \
+	Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_PRIVATE_PROPERTY_OFFSET \
+	Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_INCLASS_NO_PURE_DECLS \
+	Deathmatch_Source_Deathmatch_Public_Shotgun_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Deathmatch_Source_Deathmatch_Public_Shotgun_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
